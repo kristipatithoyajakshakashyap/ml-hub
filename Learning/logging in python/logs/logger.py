da64c76b935e2b2eef36890ceaf4852494774391
K.Thoyajaksha Kashyap <thoyajakshakashyap.kristipati@lognormal.io>
2025-10-22 18:09:08 -0500

    Data Analysis

---
+++ Learning/logging in python/logs/logger.py
@@ -0,0 +1,12 @@
+import logging
+
+for handler in logging.root.handlers[:]:
+    logging.root.removeHandler(handler)
+
+logging.basicConfig(
+    filename='app.log',
+    filemode='w',
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
